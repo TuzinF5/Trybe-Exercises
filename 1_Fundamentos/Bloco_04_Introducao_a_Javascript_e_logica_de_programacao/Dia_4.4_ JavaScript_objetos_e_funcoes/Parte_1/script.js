7df5b9a09b4d8f37143cf20dc408cf7590390c2f
Arthur Teixeira <tuzinf5@gmail.com>
2021-08-02 13:59:22 -0300

    Organiza diretórios dos dias

---
+++ 1_Fundamentos/Bloco_04_Introducao_a_Javascript_e_logica_de_programacao/Dia_4.4_ JavaScript_objetos_e_funcoes/Parte_1/script.js
@@ -0,0 +1,66 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+// Exercício 1
+
+console.log("Bem-vinda, " + info.personagem);
+
+// Exercício 2
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+console.log(info);
+
+// Exercício 3
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+for (let chaves in info) {
+  console.log(chaves);
+}
+
+// Exercício 4
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+for (let valor in info) {
+  console.log(info[valor]);
+}
+
+// Exercício 5
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+let informacao = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+for (let valor in info, informacao) {
+  console.log(info[valor] + " e " + informacao[valor]);
+}
