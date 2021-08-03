4c9fc8415ab1695c8a903849ce293c29e2aac585
Arthur Teixeira <tuzinf5@gmail.com>
2021-08-02 22:35:22 -0300

    Organiza diretórios dos dias

--- 1_Fundamentos/Bloco_04_Introducao_a_Javascript_e_logica_de_programacao/Dia_4.4_ JavaScript_objetos_e_funcoes/Parte_2/script.js
+++ 1_Fundamentos/Bloco_04_Introducao_a_Javascript_e_logica_de_programacao/Dia_4.4_ JavaScript_objetos_e_funcoes/Parte_2/script.js
@@ -49,3 +49,54 @@ function retornaMaiorValor(array) {
 console.log("O indice de menor valor é:", retornaMaiorValor(teste));
 
 // Exercício 4
+
+let nomes = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"];
+
+function maiorQuantidadeDeCaracter(array) {
+  let nome = nomes[0];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > nome.length) {
+      nome = array[index];
+    }
+  }
+  return nome;
+}
+
+console.log(maiorQuantidadeDeCaracter(nomes));
+
+// Exercício 5
+
+let numeros = [2, 3, 2, 5, 8, 2, 3];
+
+function name(params) {
+  
+}
+
+// Eercício 6
+
+let numero = 100;
+
+function somaNumeros(num) {
+  let resultado = 0;
+  for (let index = 1; index <= num; index += 1) {
+    resultado = resultado + index;
+  }
+  return resultado;
+}
+
+console.log(somaNumeros(numero));
+
+// Exercício 7
+
+let word = "trybe";
+let ending = "be";
+
+function formarFrase(word, ending) {
+  if (word > ending) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
+
+formarFrase(word, ending);
