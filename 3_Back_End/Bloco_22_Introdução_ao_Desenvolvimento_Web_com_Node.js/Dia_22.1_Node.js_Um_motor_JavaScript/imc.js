8048c5a882ad0a2d17ade621270102a12d6108fd
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-06 14:58:24 -0300

    Exercícios 4 e 5

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.1_Node.js_Um_motor_JavaScript/imc.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.1_Node.js_Um_motor_JavaScript/imc.js
@@ -1,6 +1,15 @@
 const readline = require('readline-sync');
 
-const peso = readline.question('Qual seu peso? ');
+const tabelaIMC = [
+  { IMC: 'Abaixo de 18,5', Situação: 'Abaixo do peso (magreza)' },
+  { IMC: 'Entre 18,5 e 24,9', Situação: 'Peso normal' },
+  { IMC: 'Entre 25,0 e 29,9', Situação: 'Acima do peso (sobrepeso)' },
+  { IMC: 'Entre 30,0 e 34,9', Situação: 'Obesidade grau I' },
+  { IMC: 'Entre 35,0 e 39,9', Situação: 'Obesidade grau II' },
+  { IMC: '40,0 e acima', Situação: 'Obesidade graus III e IV' },
+];
+
+const peso = readline.questionFloat('Qual seu peso? ');
 const altura = readline.question('Qual sua altura em centímetros? ');
 
 const calculaIMC = (peso, altura) => {
@@ -9,6 +18,28 @@ const calculaIMC = (peso, altura) => {
   return (peso / resultadoExponencial).toFixed(2);
 };
 
-const resultadoIMC = calculaIMC(peso, altura)
+const resultadoIMC = calculaIMC(peso, altura);
 
-console.log(`Seu IMC é: ${resultadoIMC}`);
+const categoriaIMC = (IMC) => {
+  if (IMC < 18.5) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[0].Situação}!`;
+  }
+  if (IMC >= 18.5 || IMC <= 24.9) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[1].Situação}!`;
+  }
+  if (IMC >= 25 || IMC <= 29.9) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[2].Situação}!`;
+  }
+  if (IMC >= 30 || IMC <= 34.9) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[3].Situação}!`;
+  }
+  if (IMC >= 35 || IMC <= 39.9) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[4].Situação}!`;
+  }
+  if (IMC >= 40) {
+    return `Seu IMC se enqudra em: ${tabelaIMC[5].Situação}!`;
+  }
+};
+
+console.log('\n', `Seu IMC é: ${resultadoIMC}!`);
+console.log(categoriaIMC(resultadoIMC));
