fcf91cf0c1d823e0737e47472a1f4eaf244f3367
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-06 18:17:27 -0300

    Exercício 7

---
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.1_Node.js_Um_motor_JavaScript/sorteio.js
@@ -0,0 +1,27 @@
+const readline = require('readline-sync');
+
+const numeroSorteio = () => Math.floor(Math.random() * 11);
+
+const sorteio = (numUsu, resultado) => {
+  if (numUsu === resultado) {
+    console.log('Parabéns, número correto!', '\n');
+  } else {
+    console.log(`Opa, não foi dessa vez. O número era ${resultado}`, '\n');
+  }
+};
+
+const main = () => {
+  const resultado = numeroSorteio();
+
+  const numeroUsuario = readline.questionInt('Qual será o número? ');
+
+  sorteio(numeroUsuario, resultado);
+
+  const novaTentativa = readline.keyInYNStrict('Quer tentar de novo? ');
+
+  if (novaTentativa) {
+    main();
+  }
+};
+
+main();
