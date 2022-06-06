6845469ced552d69e65aea1e15818f16cd9056f5
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-06 14:29:36 -0300

    Exercício 3

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.1_Node.js_Um_motor_JavaScript/imc.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.1_Node.js_Um_motor_JavaScript/imc.js
@@ -1,5 +1,14 @@
+const readline = require('readline-sync');
+
+const peso = readline.question('Qual seu peso? ');
+const altura = readline.question('Qual sua altura em centímetros? ');
+
 const calculaIMC = (peso, altura) => {
   const alturaEmMetro = altura / 100;
   const resultadoExponencial = Math.pow(alturaEmMetro, 2);
   return (peso / resultadoExponencial).toFixed(2);
 };
+
+const resultadoIMC = calculaIMC(peso, altura)
+
+console.log(`Seu IMC é: ${resultadoIMC}`);
