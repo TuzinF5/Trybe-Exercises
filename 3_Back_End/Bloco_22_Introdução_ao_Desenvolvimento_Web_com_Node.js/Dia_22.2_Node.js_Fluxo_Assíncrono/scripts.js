3b0d1b141bb2717a0dbb424673ae7ca0adab0549
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-08 19:51:51 -0300

    Exercício 7

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.2_Node.js_Fluxo_Assíncrono/scripts.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.2_Node.js_Fluxo_Assíncrono/scripts.js
@@ -3,7 +3,9 @@ const fs = require('fs/promises');
 
 // Exercício 6
 const escolherArquivo = async () => {
-  const caminho = readline.question('Qual o caminho do arquivo que deseja ler? ');
+  const caminho = readline.question(
+    'Qual o caminho do arquivo que deseja ler? '
+  );
 
   try {
     const conteudo = await fs.readFile(caminho, 'utf-8');
@@ -13,4 +15,33 @@ const escolherArquivo = async () => {
   }
 };
 // escolherArquivo();
+////
+
+// Exercício 7
+const substituiPalavraArquivo = async () => {
+  try {
+    const caminho = readline.question(
+      'Qual o caminho do arquivo que deseja ler? '
+    );
+    const conteudo = await fs.readFile(caminho, 'utf-8');
+
+    const palavraAntiga = readline.question('Qual palavra deseja substituir? ');
+    const palavraNova = readline.question('Qual será a nova palavra? ');
+
+    const novoconteudo = conteudo.replace(palavraAntiga, palavraNova);
+
+    console.log('O novo conteúdo do seu arquivo é: ', '\n', novoconteudo);
+
+    const caminhoDestino = readline.question(
+      'Qual o caminho para salvar o novo arquivo? '
+    );
+
+    await fs.writeFile(caminhoDestino, novoconteudo);
+
+      console.log('Arquivo salvo!');
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+// substituiPalavraArquivo();
 ////
