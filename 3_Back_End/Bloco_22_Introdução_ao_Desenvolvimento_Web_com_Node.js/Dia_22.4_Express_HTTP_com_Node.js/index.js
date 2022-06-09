77b4aa3fc37d0a7827c01b08ed1a30775e0ee073
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-09 14:10:25 -0300

    Exercício 3

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.4_Express_HTTP_com_Node.js/index.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.4_Express_HTTP_com_Node.js/index.js
@@ -14,4 +14,12 @@ app.post('/hello', (req, res) => {
   return res.status(201).json({ message: `Hello, ${name}!` });
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) return res.status(200).json({ message: `Hello, ${name}!` });
+
+  if (age <= 17) return res.status(401).json({ message: 'Unauthorized' });
+});
+
 app.listen('3000', () => console.log('App rodando na porta 3000!'));
