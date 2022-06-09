1ef985baf2e0fdd802ce6a90f0748f5481ee8c31
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-09 14:05:47 -0300

    Exercício 2

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.4_Express_HTTP_com_Node.js/index.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.4_Express_HTTP_com_Node.js/index.js
@@ -9,4 +9,9 @@ app.get('/ping', (_req, res) => {
   return res.status(200).json({ message: 'pong' });
 });
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(201).json({ message: `Hello, ${name}!` });
+});
+
 app.listen('3000', () => console.log('App rodando na porta 3000!'));
