54babc9c61ddf610593e86e482e726c41cf62530
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-10 18:37:21 -0300

    Exercício 2.1

--- 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.5_Express_Middlewares/index.js
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.5_Express_Middlewares/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const verifyProductName = require('./middlewares/verifyProductName');
 const verifyInfos = require('./middlewares/verifyInfos');
 const verifySaleDate = require('./middlewares/verifySaleDate');
 const verifyWarrantyPeriod = require('./middlewares/verifyWarrantyPeriod');
+const verifyFields = require('./middlewares/verifyFields');
 
 const app = express();
 
@@ -19,5 +21,11 @@ app.post(
     return res.status(201).json({ message: 'Venda cadastrada com sucesso' });
   }
 );
+
+app.post('/signup', verifyFields, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+
+  return res.status(200).json({ token });
+});
 
 app.listen('3001', () => console.log('App rodando na porta 3001! 🚀'));
---
+++ 3_Back_End/Bloco_22_Introdução_ao_Desenvolvimento_Web_com_Node.js/Dia_22.5_Express_Middlewares/middlewares/verifyFields.js
@@ -0,0 +1,10 @@
+const verifyFields = (req, res, next) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if (!email || !password || !firstName || !phone)
+    return res.status(401).json({ message: 'missing fields' });
+
+  return next();
+};
+
+module.exports = verifyFields;
