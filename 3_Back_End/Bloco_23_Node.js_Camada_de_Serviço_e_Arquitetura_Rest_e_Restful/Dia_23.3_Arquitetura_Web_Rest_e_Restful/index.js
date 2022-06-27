b98171a90a80c2364c95615fd8aad6e13f7058e8
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-27 12:47:28 -0300

    Exercícios do dia

---
+++ 3_Back_End/Bloco_23_Node.js_Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/Dia_23.3_Arquitetura_Web_Rest_e_Restful/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', require('./controllers/productController'));
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
---
+++ 3_Back_End/Bloco_23_Node.js_Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/Dia_23.3_Arquitetura_Web_Rest_e_Restful/models/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+require('dotenv/config');
+
+
+const { HOST_DB, USER_DB, PASSWORD_DB, DATABASE_NAME } = process.env;
+
+const connection = mysql.createPool({
+  host: HOST_DB,
+  user: USER_DB,
+  password: PASSWORD_DB,
+  database: DATABASE_NAME});
+
+module.exports = connection;
