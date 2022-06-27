278457aa2fefa525fd77ff251753ee22e1541341
Arthur Teixeira <tuzinf5@gmail.com>
2022-06-27 13:26:41 -0300

    Exercício 4

--- 3_Back_End/Bloco_23_Node.js_Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/Dia_23.3_Arquitetura_Web_Rest_e_Restful/controllers/productController.js
+++ 3_Back_End/Bloco_23_Node.js_Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/Dia_23.3_Arquitetura_Web_Rest_e_Restful/controllers/productController.js
@@ -6,35 +6,47 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const products = await ProductModel.getAll();
 
-  res.json(products);
+  if (!products.length) return res.status(404).json({ message: 'Sem produtos cadastrados!' });
+
+  res.status(200).json(products);
 });
 
 router.get('/:id', async (req, res) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.json(product);
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' });
+
+  res.status(200).json(product);
 });
 
 router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
+  if (!name || !brand) return res.status(400).json({ message: 'Os campos são obrigatórios' });
+
   const newProduct = await ProductModel.add(name, brand);
 
-  res.json(newProduct);
+  res.status(201).json(newProduct);
 });
 
 router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.json(products);
+  if (products) return res.status(404).json({ message: 'Produto não encontrado' });
+
+  res.status(204).end();
 });
 
 router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
+  if (!name || !brand) return res.status(400).json({ message: 'Os campos são obrigatórios' });
+
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.json(products);
+  if (products.affectedRows === 0) return res.status(404).json({ message: 'Produto não encontrado' });
+
+  res.status(204).end();
 });
 
 module.exports = router;
