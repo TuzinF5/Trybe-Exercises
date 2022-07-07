0c18713976b6c7572e81fcc9e1495f5870d96c3a
Arthur Teixeira <tuzinf5@gmail.com>
2022-07-07 10:59:12 -0300

    Exercício 🚀 Buscando um livro pelo id

--- 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.1_ORM_Interface_da_aplicacao_com_o_banco_de_dados/controllers/BooksController.js
+++ 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.1_ORM_Interface_da_aplicacao_com_o_banco_de_dados/controllers/BooksController.js
@@ -11,4 +11,20 @@ const getAll = async (_req, res) => {
   }
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BooksService.getById(Number(id));
+
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAll, getById };
--- 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.1_ORM_Interface_da_aplicacao_com_o_banco_de_dados/services/BooksService.js
+++ 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.1_ORM_Interface_da_aplicacao_com_o_banco_de_dados/services/BooksService.js
@@ -9,4 +9,13 @@ const getAll = async () => {
   }
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  try {
+    const books = await Book.findByPk(id);
+    return books;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = { getAll, getById };
