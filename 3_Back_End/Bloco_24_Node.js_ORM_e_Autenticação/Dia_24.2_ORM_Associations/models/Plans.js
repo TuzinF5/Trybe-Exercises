5df0f05235ba68513fa13635806d5e964bfcf003
Arthur Teixeira <tuzinf5@gmail.com>
2022-07-08 15:34:13 -0300

    Exercício 1

---
+++ 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.2_ORM_Associations/models/Plans.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Plans = (sequelize, DataTypes) => {
+  const Plans = sequelize.define(
+    'Plans',
+    {
+      plan_id: { type: DataTypes.INTEGER, primaryKey: true },
+      coverage: DataTypes.STRING,
+      price: DataTypes.DOUBLE,
+    },
+    {
+      timestamps: false,
+      tableName: 'Plans',
+    }
+  );
+
+  Plans.associate = (models) => {
+    Plans.hasMany(models.Patients, { foreignKey: 'plan_id', as: 'patients' });
+  };
+
+  return Plans;
+};
+
+module.exports = Plans;
