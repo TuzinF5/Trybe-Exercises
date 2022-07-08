a3fdba4c327bf4cc8831a3cc2cd845752140eb55
Arthur Teixeira <tuzinf5@gmail.com>
2022-07-08 15:34:23 -0300

    Exercício 2

---
+++ 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.2_ORM_Associations/models/Patients.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Patients = (sequelize, DataTypes) => {
+  const Patients = sequelize.define(
+    'Patients',
+    {
+      patient_id: { type: DataTypes.INTEGER, primaryKey: true },
+      fullname: DataTypes.STRING,
+      plan_id: { type: DataTypes.INTEGER, foreignKey: true },
+    },
+    {
+      timestamps: false,
+      tableName: 'Patients',
+    }
+  );
+
+  Patients.associate = (models) => {
+    Patients.belongsTo(models.Plans, { foreignKey: 'plan_id', as: 'plan' });
+  };
+
+  return Patients;
+};
+
+module.exports = Patients;
