9b9e9813b4ee14314c975572c583d145d326e9a3
Arthur Teixeira <tuzinf5@gmail.com>
2022-07-08 15:43:39 -0300

    Exercício 6

--- 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.2_ORM_Associations/index.js
+++ 3_Back_End/Bloco_24_Node.js_ORM_e_Autenticação/Dia_24.2_ORM_Associations/index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 
-const { Patients, Plans } = require('./models');
+const { Patients, Plans, Surgeries } = require('./models');
 
 const app = express();
 app.use(express.json());
@@ -16,6 +16,28 @@ app.get('/patients', async (_req, res) => {
       attributes: { exclude: ['plan_id'] },
       order: [['patient_id', 'ASC']],
       include: { model: Plans, as: 'plan' },
+    });
+
+    if (!patients.length) {
+      return res.status(404).json({ message: 'No patients found' });
+    }
+
+    return res.status(200).json(patients);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+app.get('/patients-surgeries', async (_req, res) => {
+  try {
+    const patients = await Patients.findAll({
+      // attributes: { exclude: ['plan_id'] },
+      order: [['patient_id', 'ASC']],
+      include: {
+        model: Surgeries,
+        as: 'surgeries',
+        through: { attributes: [] },
+      },
     });
 
     if (!patients.length) {
