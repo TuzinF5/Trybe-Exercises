92157de0d3728184d81ad96c16076789b40b47f8
Arthur Teixeira <tuzinf5@gmail.com>
2022-10-17 13:36:05 -0300

    Exercício 2

--- 4_Ciencia_da_Computacao/Secao_1_Introducao_a_Python/Dia_1_Aprendendo_Python/exercicios.py
+++ 4_Ciencia_da_Computacao/Secao_1_Introducao_a_Python/Dia_1_Aprendendo_Python/exercicios.py
@@ -1,6 +1,17 @@
+import statistics
+
+
 # Exercicio 1
 def returns_greater_number(num1, num2):
     return max(num1, num2)
 
 
 print(returns_greater_number(31, 30))
+
+
+# Exercicio 2
+def calculate_the_average(list_of_numbers):
+    return statistics.mean(list_of_numbers)
+
+
+print(calculate_the_average([2, 4, 6, 8, 10]))
