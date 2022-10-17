cfa76329eab2efada9cfd8097db152c66378e3f1
Arthur Teixeira <tuzinf5@gmail.com>
2022-10-17 13:07:15 -0300

    Nova configuração de conexão

--- 3_Back_End/Bloco_30_MongoDB_com_Node.js_e_POO/Dia_30.2_Mongoose_e_arquitetura_MSC_camada_Service_e_Controller/src/models/Connection.ts
+++ 3_Back_End/Bloco_30_MongoDB_com_Node.js_e_POO/Dia_30.2_Mongoose_e_arquitetura_MSC_camada_Service_e_Controller/src/models/Connection.ts
@@ -10,11 +10,13 @@ import mongoose from 'mongoose';
 //   dbName: 'glassesStore', // Define qual banco de dados vou utilizar.
 // };
 
-// connect('mongodb://localhost:27017/', options);
+// mongoose.connect('mongodb://localhost:27017/', options);
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
+    // || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin',
+    || `mongodb://127.0.0.1:27017/
+    ?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1`,
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
