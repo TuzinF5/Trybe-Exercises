cd204f2fd2804c78968c576dbef50d0eb12b2c6c
Arthur Teixeira <tuzinf5@gmail.com>
2022-11-05 12:46:52 -0300

    Exercício 2

--- 4_Ciencia_da_Computacao/Secao_3_Redes_e_Raspagem_de_Dados/Dia_2_Raspagem_de_Dados/exercicios.py
+++ 4_Ciencia_da_Computacao/Secao_3_Redes_e_Raspagem_de_Dados/Dia_2_Raspagem_de_Dados/exercicios.py
@@ -3,3 +3,9 @@ import requests
 # Exercicio 1
 response = requests.get("https://httpbin.org/encoding/utf8")
 print(response.text)
+
+# Exercicio 2
+response = requests.get("https://api.github.com/users")
+users = response.json()
+for user in users:
+    print(user["login"], user["url"])
