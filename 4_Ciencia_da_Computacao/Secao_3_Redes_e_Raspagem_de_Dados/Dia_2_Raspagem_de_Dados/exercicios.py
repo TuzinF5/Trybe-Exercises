f7a37f09c689f42466a9b18db361fe19fb2ddce9
Arthur Teixeira <tuzinf5@gmail.com>
2022-11-05 12:57:01 -0300

    Exercício 3

--- 4_Ciencia_da_Computacao/Secao_3_Redes_e_Raspagem_de_Dados/Dia_2_Raspagem_de_Dados/exercicios.py
+++ 4_Ciencia_da_Computacao/Secao_3_Redes_e_Raspagem_de_Dados/Dia_2_Raspagem_de_Dados/exercicios.py
@@ -9,3 +9,11 @@ response = requests.get("https://api.github.com/users")
 users = response.json()
 for user in users:
     print(user["login"], user["url"])
+
+# Exercicio 3
+response = requests.get(
+    "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+    headers={"User-agent": "Mozilla", "Accept": "text/html"},
+)
+print(response.text)
+assert "bot detected" not in response.text
